05bdc035aaa0050504a2bb55c46e87d68c72d214
CMK <cirno.mainasuk@gmail.com>
2020-09-01 11:15:53 +0800

    git init

---
+++ TwidereX/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwidereX
+//
+//  Created by Cirno MainasuK on 2020-8-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
