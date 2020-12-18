d41cc7fdddf3582df885c34260526929f2ce6fa8
CMK <cirno.mainasuk@gmail.com>
2020-12-18 18:03:15 +0800

    chore: remove DEBUG window

--- StubMixer/SceneDelegate.swift
+++ StubMixer/SceneDelegate.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    // var window: UIWindow?
-    var window: UIWindow? = TestWindow()
-
+    var window: UIWindow?
+    
+    // #if DEBUG
+    // var window: UIWindow? = TestWindow()
+    // #endif
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
