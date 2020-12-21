74200cb0264e0dc631e18a863fc18f6852d42938
CMK <cirno.mainasuk@gmail.com>
2020-12-21 17:43:43 +0800

    fix: scrollRectToVisible not works when large dataset issue. resolve: #15

--- TwidereX/Scene/HomeTimeline/HomeTimelineViewController.swift
+++ TwidereX/Scene/HomeTimeline/HomeTimelineViewController.swift
@@ -656,7 +656,8 @@ extension HomeTimelineViewController: ScrollViewContainer {
     var scrollView: UIScrollView { return tableView }
     
     func scrollToTop(animated: Bool) {
-        if viewModel.loadLatestStateMachine.canEnterState(HomeTimelineViewModel.LoadLatestState.Loading.self),
+        if scrollView.contentOffset.y < scrollView.frame.height,
+           viewModel.loadLatestStateMachine.canEnterState(HomeTimelineViewModel.LoadLatestState.Loading.self),
            (scrollView.contentOffset.y + scrollView.adjustedContentInset.top) == 0.0,
            !refreshControl.isRefreshing {
             scrollView.scrollRectToVisible(CGRect(origin: CGPoint(x: 0, y: -refreshControl.frame.height), size: CGSize(width: 1, height: 1)), animated: animated)
@@ -666,7 +667,9 @@ extension HomeTimelineViewController: ScrollViewContainer {
                 self.refreshControl.sendActions(for: .valueChanged)
             }
         } else {
-            scrollView.scrollRectToVisible(CGRect(origin: .zero, size: CGSize(width: 1, height: 1)), animated: animated)
+            let indexPath = IndexPath(row: 0, section: 0)
+            guard viewModel.diffableDataSource?.itemIdentifier(for: indexPath) != nil else { return }
+            tableView.scrollToRow(at: indexPath, at: .top, animated: true)
         }
     }
     
