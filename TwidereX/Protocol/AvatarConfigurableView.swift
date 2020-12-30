de3ff4f7795811b3f363ffd9a08c1c03a0b850cf
CMK <cirno.mainasuk@gmail.com>
2020-12-30 16:27:04 +0800

    feat: add Me profile scene coordinator for drawer header. Update coordinator present logic

--- TwidereX/Protocol/AvatarConfigurableView.swift
+++ TwidereX/Protocol/AvatarConfigurableView.swift
@@ -11,55 +11,89 @@ import AlamofireImage
 import Kingfisher
 
 protocol AvatarConfigurableView {
-    static var avatarImageViewSize: CGSize { get }
-    var avatarImageView: UIImageView { get }
-    var verifiedBadgeImageView: UIImageView { get }
+    static var configurableAvatarImageViewSize: CGSize { get }
+    var configurableAvatarImageView: UIImageView? { get }
+    var configurableAvatarButton: UIButton? { get }
+    var configurableVerifiedBadgeImageView: UIImageView? { get }
     func configure(avatarImageURL: URL?, verified: Bool)
 }
 
 extension AvatarConfigurableView {
     public func configure(avatarImageURL: URL?, verified: Bool = false) {
         // set verified
-        verifiedBadgeImageView.isHidden = !verified
-        
-        // cancel previous task
-        avatarImageView.af.cancelImageRequest()
-        avatarImageView.kf.cancelDownloadTask()
+        configurableVerifiedBadgeImageView?.isHidden = !verified
         
         let placeholderImage = UIImage
-            .placeholder(size: Self.avatarImageViewSize, color: .systemFill)
+            .placeholder(size: Self.configurableAvatarImageViewSize, color: .systemFill)
             .af.imageRoundedIntoCircle()
+        
+        
+        // cancel previous task
+        configurableAvatarImageView?.af.cancelImageRequest()
+        configurableAvatarImageView?.kf.cancelDownloadTask()
+        configurableAvatarButton?.af.cancelImageRequest(for: .normal)
+        configurableAvatarButton?.kf.cancelImageDownloadTask()
         
         // set placeholder if no asset
         guard let avatarImageURL = avatarImageURL else {
-            avatarImageView.image = placeholderImage
+            configurableAvatarImageView?.image = placeholderImage
+            configurableAvatarButton?.setImage(placeholderImage, for: .normal)
             return
         }
+
+        if let avatarImageView = configurableAvatarImageView {
+            // set avatar (GIF using Kingfisher)
+            switch avatarImageURL.pathExtension {
+            case "gif":
+                avatarImageView.kf.setImage(
+                    with: avatarImageURL,
+                    placeholder: placeholderImage,
+                    options: [
+                        .processor(
+                            CroppingImageProcessor(size: Self.configurableAvatarImageViewSize, anchor: CGPoint(x: 0.5, y: 0.5)) |>
+                                RoundCornerImageProcessor(cornerRadius: 0.5 * Self.configurableAvatarImageViewSize.width)
+                        ),
+                        .transition(.fade(0.2))
+                    ]
+                )
+            default:
+                let filter = ScaledToSizeCircleFilter(size: Self.configurableAvatarImageViewSize)
+                avatarImageView.af.setImage(
+                    withURL: avatarImageURL,
+                    placeholderImage: placeholderImage,
+                    filter: filter,
+                    imageTransition: .crossDissolve(0.3),
+                    runImageTransitionIfCached: false,
+                    completion: nil
+                )
+            }
+        }
         
-        // set avatar (GIF using Kingfisher)
-        switch avatarImageURL.pathExtension {
-        case "gif":
-            avatarImageView.kf.setImage(
-                with: avatarImageURL,
-                placeholder: placeholderImage,
-                options: [
-                    .processor(
-                        CroppingImageProcessor(size: Self.avatarImageViewSize, anchor: CGPoint(x: 0.5, y: 0.5)) |>
-                        RoundCornerImageProcessor(cornerRadius: 0.5 * Self.avatarImageViewSize.width)
-                    ),
-                    .transition(.fade(0.2))
-                ]
-            )
-        default:
-            let filter = ScaledToSizeCircleFilter(size: Self.avatarImageViewSize)
-            avatarImageView.af.setImage(
-                withURL: avatarImageURL,
-                placeholderImage: placeholderImage,
-                filter: filter,
-                imageTransition: .crossDissolve(0.3),
-                runImageTransitionIfCached: false,
-                completion: nil
-            )
+        if let avatarButton = configurableAvatarButton {
+            switch avatarImageURL.pathExtension {
+            case "gif":
+                avatarButton.kf.setImage(
+                    with: avatarImageURL,
+                    for: .normal,
+                    placeholder: placeholderImage,
+                    options: [
+                        .processor(
+                            CroppingImageProcessor(size: Self.configurableAvatarImageViewSize, anchor: CGPoint(x: 0.5, y: 0.5)) |>
+                                RoundCornerImageProcessor(cornerRadius: 0.5 * Self.configurableAvatarImageViewSize.width)
+                        ),
+                        .transition(.fade(0.2))
+                    ]
+                )
+            default:
+                let filter = ScaledToSizeCircleFilter(size: Self.configurableAvatarImageViewSize)
+                avatarButton.af.setImage(
+                    for: .normal,
+                    url: avatarImageURL,
+                    placeholderImage: placeholderImage,
+                    filter: filter,
+                    completion: nil
+                )
+            }
         }
     }
 }
