5072c5ada329c3e156e28044b69964914b522a17
CMK <cirno.mainasuk@gmail.com>
2021-07-09 10:54:25 +0800

    feat: add name and photo media for post

--- TwidereX/Scene/HomeTimeline/HomeTimelineView.swift
+++ TwidereX/Scene/HomeTimeline/HomeTimelineView.swift
@@ -13,7 +13,10 @@ import Kingfisher
 
 struct HomeTimelineView: View {
 
-    @ScaledMetric(relativeTo: .headline) var avatarSize: CGFloat = 44
+    @ScaledMetric(relativeTo: .headline) var _avatarSize: CGFloat = 44.0
+    var avatarSize: CGFloat {
+        return max(44.0, min(88.0, _avatarSize))
+    }
 
     @FetchRequest(
         sortDescriptors: TimelineIndex.defaultSortDescriptors,
@@ -25,34 +28,58 @@ struct HomeTimelineView: View {
     var body: some View {
         List {
             ForEach(indexes, id: \.self) { index in
-                HStack {
-                    AsyncImage(url: index.tweet?.author.avatarImageURL()) { phase in
-                        switch phase {
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .transition(.opacity)
-                        case .failure, .empty:
-                            Color.gray
-                        @unknown default:
-                            Color.gray
-                        }
-                    }
-                    .clipShape(Circle())
-                    .frame(width: avatarSize, height: avatarSize)
-                    Spacer(minLength: 10)
-                    KFImage(index.tweet?.author.avatarImageURL())
+                let avatarImageURL = index.tweet?.author.avatarImageURL()
+                let name = index.tweet?.author.name ?? "-"
+                let username = index.tweet?.author.username ?? "-"
+                let content = index.tweet?.text ?? ""
+                let media = Array((index.tweet?.retweet ?? index.tweet)?.media ?? []).sorted { $0.index.compare($1.index) == .orderedAscending }
+                let imageMedia = media.filter { $0.type == "photo" }
+                HStack(alignment: .top) {
+                    KFImage(avatarImageURL)
                         .placeholder {
                             Color.gray
                         }
                         .cacheOriginalImage()
+                        .cancelOnDisappear(true)
                         .fade(duration: 0.2)
                         .forceTransition()
                         .resizable()
                         .frame(width: avatarSize, height: avatarSize)
                         .clipShape(Circle())
-                    Spacer()
+                    VStack(alignment: .leading, spacing: 10) {
+                        VStack(alignment: .leading) {
+                            Text(name)
+                                .font(.headline)
+                            Text("@" + username)
+                                .font(.subheadline)
+                        }
+                        Text(content)
+                            .font(.body)
+                        ForEach(imageMedia, id: \.id) { media in
+                            let photoURL = media.url.flatMap { URL(string: $0) }
+                            let aspectRatio: CGFloat? = {
+                                guard let width = media.width?.intValue,
+                                      let height = media.height?.intValue else { return nil }
+                                return CGFloat(width) / CGFloat(height)
+                            }()
+                            KFImage(photoURL)
+                                .placeholder {
+                                    Color.gray
+                                }
+                                .cacheOriginalImage()
+                                .cancelOnDisappear(true)
+                                .fade(duration: 0.2)
+                                .forceTransition()
+                                .resizable()
+                                .aspectRatio(aspectRatio, contentMode: .fit)
+                                .frame(maxHeight: floor(UIScreen.main.bounds.height / 3))
+                                .cornerRadius(10)
+                                .onTapGesture(count: 1) {
+                                    let photoURLString = photoURL?.absoluteString ?? "<nil>"
+                                    print("image: \(photoURLString)")
+                                }
+                        }
+                    }
                 }
             }
         }
