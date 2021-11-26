83401780d784402baafd3d1c529613992597eb1e
CMK <cirno.mainasuk@gmail.com>
2021-11-26 18:32:35 +0800

    fix: set image asset for SwiftUI without bundle issue

---
+++ TwidereSDK/Sources/TwidereAsset/TwidereAsset+Bundle.swift
@@ -0,0 +1,14 @@
+//
+//  TwidereAsset+Bundle.swift
+//  
+//
+//  Created by MainasuK on 2021-11-26.
+//
+
+import Foundation
+
+public enum TwidereAsset {
+    public static var bundle: Bundle {
+        Bundle.module
+    }
+}
