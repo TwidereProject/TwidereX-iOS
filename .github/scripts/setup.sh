c685fcff5d56f90647fa5fa3262bcae85d110a20
CMK <cirno.mainasuk@gmail.com>
2022-03-28 19:05:50 +0800

    chore: update setup scripts

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -3,11 +3,11 @@
 sudo gem install cocoapods-keys
 
 # set "TwidereX" project name to make cocoapods-keys using the right project
-pod keys set app_secret ${{ secrets.APP_SECRET }} "TwidereX"
-pod keys set consumer_key ${{ secrets.CONSUMER_KEY }} "TwidereX"
-pod keys set consumer_key_secret ${{ secrets.CONSUMER_KEY_SECRET }} "TwidereX"
-pod keys set host_key_public ${{ secrets.HOST_KEY_PUBLIC }} "TwidereX"
-pod keys set oauth_endpoint ${{ secrets.OAUTH_ENDPOINT }} "TwidereX"
+pod keys set app_secret ${APP_SECRET} "TwidereX"
+pod keys set consumer_key ${CONSUMER_KEY} "TwidereX"
+pod keys set consumer_key_secret ${CONSUMER_KEY_SECRET} "TwidereX"
+pod keys set host_key_public ${HOST_KEY_PUBLIC} "TwidereX"
+pod keys set oauth_endpoint ${OAUTH_ENDPOINT} "TwidereX"
 pod keys set oauth_endpoint_debug "oob" "TwidereX"
 
 pod install
