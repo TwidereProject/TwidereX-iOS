d2aa554a48dd12e2260f4fab8ea53c69f945a01f
CMK <cirno.mainasuk@gmail.com>
2023-03-23 21:03:04 +0800

    fix: custom emoji attachment not display for user nickname issue

--- TwidereSDK/Sources/TwidereUI/UIViewRepresentable/LabelRepresentable.swift
+++ TwidereSDK/Sources/TwidereUI/UIViewRepresentable/LabelRepresentable.swift
@@ -9,72 +9,42 @@ import UIKit
 import SwiftUI
 import TwidereCore
 import MetaTextKit
+import MetaLabel
 
 public struct LabelRepresentable: UIViewRepresentable {
     
-    let label: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 1
-        label.backgroundColor = .clear
-        label.adjustsFontSizeToFitWidth = false
-        label.allowsDefaultTighteningForTruncation = false
-        label.lineBreakMode = .byTruncatingTail
-        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)     // always try grow vertical
-        label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        return label
-    }()
+    let label: MetaLabel
     
     // input
-    public let metaContent: MetaContent
-    public let textStyle: TextStyle
-    let setupLabel: (UILabel) -> Void
+    let metaContent: MetaContent
+    let textStyle: TextStyle
+    let setupLabel: (MetaLabel) -> Void
     
     public init(
         metaContent: MetaContent,
         textStyle: TextStyle,
-        setupLabel: @escaping (UILabel) -> Void
+        setupLabel: @escaping (MetaLabel) -> Void
     ) {
         self.metaContent = metaContent
         self.textStyle = textStyle
         self.setupLabel = setupLabel
+        self.label = {
+            let label = MetaLabel(style: textStyle)
+            label.textArea.textContainer.lineBreakMode = .byTruncatingTail
+            label.setContentHuggingPriority(.defaultHigh, for: .horizontal)     // always try grow vertical
+            label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            return label
+        }()
     }
     
-    public func makeUIView(context: Context) -> UILabel {
-        let label = self.label
+    public func makeUIView(context: Context) -> UIView {
         setupLabel(label)
-        
-        let attributedString = NSMutableAttributedString(string: metaContent.string)
-        let textAttributes: [NSAttributedString.Key: Any] = [
-            .font: textStyle.font,
-            .foregroundColor: textStyle.textColor,
-        ]
-        let linkAttributes: [NSAttributedString.Key: Any] = [
-            .font: textStyle.font,
-            .foregroundColor: UIColor.tintColor,
-        ]
-        let paragraphStyle: NSMutableParagraphStyle = {
-            let style = NSMutableParagraphStyle()
-            let fontMargin = textStyle.font.lineHeight - textStyle.font.pointSize
-            style.lineSpacing = 3 - fontMargin
-            style.paragraphSpacing = 8 - fontMargin
-            return style
-        }()
-        
-        MetaText.setAttributes(
-            for: attributedString,
-            textAttributes: textAttributes,
-            linkAttributes: linkAttributes,
-            paragraphStyle: paragraphStyle,
-            content: metaContent
-        )
-        
-        label.attributedText = attributedString
-        
+        label.configure(content: metaContent)
         return label
     }
     
-    public func updateUIView(_ view: UILabel, context: Context) {
-        
+    public func updateUIView(_ view: UIView, context: Context) {
+        // do nothing
     }
     
     public func makeCoordinator() -> Coordinator {
